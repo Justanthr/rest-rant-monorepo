673695027e1f6d38e2cb025713e04227b60bf69a
Jennika Wilson <j.r.a.112100@gmail.com>
2022-08-13 09:49:05 -0400

    Front end done

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react"
-import { useHistory, useParams } from "react-router"
+import { useState  } from "react"
+import { useHistory } from "react-router"
 
 function SignUpForm() {
 
@@ -65,6 +65,18 @@ function SignUpForm() {
 							className="form-control"
 							id="email"
 							name="email"
+						/>
+					</div>
+					<div className="col-sm-6 form-group">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							required
+							value={user.password}
+							onChange={e => setUser({ ...user, password: e.target.value })}
+							className="form-control"
+							id="password"
+							name="password"
 						/>
 					</div>
 				</div>
